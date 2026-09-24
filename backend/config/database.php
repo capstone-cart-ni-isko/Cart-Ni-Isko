3157a177d7fb56d4027471a704ff499b432a6991
Austin Doki <abf2023-2637-49119@bicol-u.edu.ph>
2026-09-24 22:03:47 +0800

    Allow DB_SSLMODE to be configured via environment variable

--- backend/config/database.php
+++ backend/config/database.php
@@ -96,7 +96,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'prefer', // <--- ADD THIS LINE HERE
+            'sslmode' => env('DB_SSLMODE', 'prefer'),
         ],
 
         'sqlsrv' => [
